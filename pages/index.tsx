408604aecea244b888f6f4ab43541738830cf7c4
Charlie Depman <cdepman@gmail.com>
2023-11-08 21:52:15 -0500

    some clean up, added content

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ export default function Web() {
               <Button
                 href="https://github.com/Blazity/next-enterprise"
                 className="mr-3"
-                style={{ backgroundColor: "#30b5a6" }}
+                style={{ backgroundColor: "#30b5a6", border: "none" }}
               >
                 Get the App
               </Button>
@@ -100,7 +100,27 @@ export default function Web() {
                 {singleItem.list && singleItem.list.length && (
                   <ul className="list-disc text-left text-gray-500 dark:text-gray-400">
                     {singleItem.list?.map((listItem) => (
-                      <li key={singleItem.title}>{listItem}</li>
+                      <li
+                        key={singleItem.title}
+                        className="flex items-center space-x-3"
+                      >
+                        <svg
+                          class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 16 12"
+                        >
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 5.917 5.724 10.5 15 1.5"
+                          />
+                        </svg>
+                        <span>{listItem}</span>
+                      </li>
                     ))}
                   </ul>
                 )}
