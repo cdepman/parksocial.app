67805708949ef543d558e58e0a390b9ecdf86df1
Charlie Depman <charliedepman@macbook-air.mynetworksettings.com>
2023-11-09 22:17:24 -0500

    soften background color

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Web() {
         <title>ParkSocial</title>
       </Head>
       <Switcher />
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-gray-100 dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center pt-10">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
@@ -79,7 +79,7 @@ export default function Web() {
           </div>
         </div>
       </section>
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-gray-100 dark:bg-gray-900">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
           <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
             {LP_GRID_ITEMS.map((singleItem) => (
